40bb930c9fac31b1a034a0700cfd09fe3fa96e79
gogyrs <155965944+gogyrs@users.noreply.github.com>
2024-01-08 12:51:33 +0200

    Add files via upload

---
+++ Lectures/01.PB-Python-First-Steps-in-Coding/07. Projects_Creation.py
@@ -0,0 +1,4 @@
+arch_name = input()
+proj_count = int(input())
+hours_to_complete = proj_count * 3
+print(f'The architect {arch_name} will need {hours_to_complete} hours to complete {proj_count} project/s.')
---
+++ Lectures/01.PB-Python-First-Steps-in-Coding/08. Pet_Shop.py
@@ -0,0 +1,4 @@
+dog_food_count = int(input())
+cat_food_count = int(input())
+total_amount = dog_food_count * 2.5 + cat_food_count * 4
+print(f'{total_amount} lv.')
